9922964651679482dab539c4e9dcbf4107c7d96d
karatosun8 <sefa_karatosun@hotmail.com>
2023-08-22 10:50:48 +0300

    start BlogHome

---
+++ blog-web-site/src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import BlogHome from "./components/Blog/BlogHome";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <BlogHome/>
+    </div>
+  );
+}
+
+export default App;
