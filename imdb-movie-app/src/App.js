09d2a07c01af924368880afffcf2fee25b5c0878
karatosun8 <sefa_karatosun@hotmail.com>
2023-08-26 22:39:10 +0300

    home

--- imdb-movie-app/src/App.js
+++ imdb-movie-app/src/App.js
@@ -2,15 +2,18 @@
 import './App.css';
 import { BrowserRouter as Router,Routes,Route
 } from "react-router-dom";
+import Header from './components/header/Header';
+import Home from './pages/home/home'
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
-          <Route index element={<h1>Hello WOrld'</h1>}></Route>
+          <Route index element={<Home/>}></Route>
           <Route path='movie/:id' element={<h1>Movie detail page</h1>}></Route>
-          <Route path='movie/:type' element={<h1>Movie detail page</h1>}></Route>
+          <Route path='movies/:type' element={<h1>Movie detail page</h1>}></Route>
           <Route path='/*' element={<h1>Error Page</h1>}></Route>
         </Routes>
       </Router>
