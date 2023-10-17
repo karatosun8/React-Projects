722bdb224e9dfa6043db49d91e911e765d0bcac3
karatosun8 <sefa_karatosun@hotmail.com>
2023-10-17 13:36:07 +0300

    taskList

--- react-task/src/App.jsx
+++ react-task/src/App.jsx
@@ -2,17 +2,27 @@
 import './App.css'
 import TaskCreate from './components/TaskCreate'
 import TaskList from './components/TaskList'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([])
 
   const createTask = (title,taskDesc) =>{
-    console.log(title,taskDesc);
+  const createdTasks = [
+    ...tasks,{
+      id:Math.round(Math.random()*9999999),
+      title : title,
+      taskDesc : taskDesc
+    }
+  ];
+  setTasks(createdTasks)
+
   };
   return (
     <div className='App'> 
     <TaskCreate onCreate={createTask}/>
     <h1>Görevler</h1>
-    <TaskList/>   
+    <TaskList tasks ={tasks}/>   
     </div>
   )
 }
--- react-task/src/components/TaskList.jsx
+++ react-task/src/components/TaskList.jsx
@@ -1,6 +1,17 @@
-function TaskList (){
+
+import TaskShow from "./TaskShow"
+
+
+function TaskList ({tasks}){
     return (
-        <div></div>
+        <div>
+            {tasks.map((task,index)=>{
+                return(
+                    <TaskShow key={index} task={task} />
+                )
+            })}
+
+        </div>
     )
 }
 export default TaskList
