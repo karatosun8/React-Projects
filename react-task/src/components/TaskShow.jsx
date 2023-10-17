41b62e203043d2702a5df72b5ee3a8d1dc133138
karatosun8 <sefa_karatosun@hotmail.com>
2023-10-17 19:39:05 +0300

    delete

--- react-task/src/components/TaskShow.jsx
+++ react-task/src/components/TaskShow.jsx
@@ -1,6 +1,9 @@
 
-function TaskShow ({task}) {
-    console.log(task);
+function TaskShow ({task,onDelete}) {
+    // console.log(task);
+    const handleDeleteClick = ()=>{
+        onDelete(task.id)
+    };
     return(
         <div className="task-show">
             <h3 className="task-title">Göreviniz</h3>
@@ -8,7 +11,7 @@ function TaskShow ({task}) {
             <h3 className="task-title">Yapılacaklar</h3>
             <p>{task.taskDesc}</p>
             <div>
-                <button className="task-delete">Sil</button>
+                <button className="task-delete" onClick={handleDeleteClick}>Sil</button>
                 <button className="task-edit">Güncelle</button>
             </div>
 
